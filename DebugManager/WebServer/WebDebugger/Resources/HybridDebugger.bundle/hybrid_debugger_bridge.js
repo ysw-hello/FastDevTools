a7b00ef172bddb94b8f3712858d7a234a6201760
yanshiwei <yanshiwei@zuoyebang.com>
2019-12-19 14:52:28 +0800

    参数兼容性处理

--- DebugManager/WebServer/WebDebugger/Resources/HybridDebugger.bundle/hybrid_debugger_bridge.js
+++ DebugManager/WebServer/WebDebugger/Resources/HybridDebugger.bundle/hybrid_debugger_bridge.js
@@ -5,12 +5,30 @@
     
     var callbackPool = {};
     var ack_no = 1;
+    
+    window.debuggerBridge.isJson = function(obj) {
+        var isjson = typeof(obj) == "object" && Object.prototype.toString.call(obj).toLowerCase() == "[object object]" && !obj.length;
+        return isjson;
+    }
     window.debuggerBridge.invoke = function(_action, _data, _callback) {
         var rndKey = 'cbk_' + new Date().getTime();
+        
+        //兼容处理 _data与_callback顺序混乱的问题
+        if (!debuggerBridge.isjson(_data)) {
+            var third = _data;
+            if (debuggerBridge.isjson(_callback)) {
+                _data = _callback;
+            }
+            if (typeof third == 'function') {
+                _callback = third;
+            }
+        }
+        
         var fullParam = {
             action: _action,
             param: _data
         };
+        
         if (_callback) { //如果有回调函数。
             var rndKey = 'cbk_' + ack_no++;
             fullParam.callbackKey = rndKey;
